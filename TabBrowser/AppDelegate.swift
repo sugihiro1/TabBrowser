9b0f014359269bc79500a7114a667089e19fbc5d
sugihiro1 <31731151+sugihiro1@users.noreply.github.com>
2017-11-14 00:25:48 +0900

    2017/11/13 version

--- TabBrowser/AppDelegate.swift
+++ TabBrowser/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import SwiftyDropbox
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+ 
+    DropboxClientsManager.setupWithAppKey("8yhy6e69tiewf1c")
     
 //    let viewController: BrowserVC = BrowserVC()
     let viewController: TabVC = TabVC()
@@ -28,6 +32,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     return true
   }
 
+  func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+    if let authResult = DropboxClientsManager.handleRedirectURL(url) {
+      switch authResult {
+      case .success:
+        print("Success! User is logged into Dropbox.")
+      case .cancel:
+        print("Authorization flow was manually canceled by user!")
+      case .error(_, let description):
+        print("Error: \(description)")
+      }
+    }
+    return true
+  }
+
+  
   func applicationWillResignActive(_ application: UIApplication) {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
--- TabBrowser/ViewController.swift
+++ TabBrowser/ViewController.swift
@@ -1,25 +1,76 @@
 //
 //  ViewController.swift
-//  TabBrowser
+//  Vietnam2
 //
-//  Created by 杉山尋美 on 2017/11/06.
+//  Created by 杉山尋美 on 2017/09/28.
 //  Copyright © 2017年 hiromi.sugiyama. All rights reserved.
 //
 
 import UIKit
+import WebKit
+import SwiftyDropbox
+import SVProgressHUD
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKUIDelegate, UIWebViewDelegate {
+  
+  @IBOutlet weak var BackButton: UIBarButtonItem!
+  
+  var webView: UIWebView = UIWebView()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.webView.delegate = self
+    self.webView.scalesPageToFit = true
+    
+    self.webView.frame = self.view.bounds
+    self.view.addSubview(self.webView)
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+    
   }
-
-
+  
+  // SearchWord クラスからBackボタンで戻って来た時再描画
+  override func viewDidAppear(_ animated: Bool) {
+    showLocalHtml()
+  }
+  
+  
+  // ローカルHtmlファイルのweb表示プロシージャ
+  func showLocalHtml() {
+    
+    // Documentファルダにあるmenuファイルのフルパスを取得
+    let fileManager = FileManager.default
+    let documentURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+//    let dataURL = documentURL.appendingPathComponent("Data")
+    let menuURL = documentURL.appendingPathComponent("wordsmenu2Iphone.htm")
+    
+    let path = menuURL.path   // String型の Document path
+    let url = NSURL(fileURLWithPath: path)
+    let urlRequest = NSURLRequest(url: url as URL)
+    self.webView.loadRequest(urlRequest as URLRequest)
+    
+  }
+  
+  // HTMLで遷移した時の「戻る」ボタン状態と色
+  func webViewDidFinishLoad(_ webView: UIWebView) {
+    if webView.canGoBack {
+      BackButton.isEnabled = true
+    } else {
+      BackButton.isEnabled = false
+    }
+  }
+  
+  // HTMLで遷移した時の「戻る」操作
+  @IBAction func Return(_ sender: Any) {
+    self.webView.goBack()
+  }
+  
+  
 }
+
+
+
 
